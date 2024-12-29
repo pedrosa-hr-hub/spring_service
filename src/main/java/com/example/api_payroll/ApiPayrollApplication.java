4b643c6fbd063e3461f578ab6accec8e0005427f
Pedro de Sá <pedro.trabalhos@outlook.com>
2024-12-28 23:11:30 -0300

    initialize spring aplication

---
+++ src/main/java/com/example/api_payroll/ApiPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_payroll/ApiPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
