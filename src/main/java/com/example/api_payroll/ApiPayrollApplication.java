77a97cf4a5a3865149999272121ca4cd33f75a2e
Pedro de Sá <pedro.trabalhos@outlook.com>
2024-12-28 23:24:21 -0300

    dot env confing

--- src/main/java/com/example/api_payroll/ApiPayrollApplication.java
+++ src/main/java/com/example/api_payroll/ApiPayrollApplication.java
@@ -3,10 +3,18 @@ package com.example.api_payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiPayrollApplication {
 
 	public static void main(String[] args) {
+		// .env config
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		// main
 		SpringApplication.run(ApiPayrollApplication.class, args);
 	}
 
